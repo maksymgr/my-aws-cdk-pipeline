9edec2dee9a30265ad7988849745cfcc6731b61e
Maksym Gryevtsov <maksymg@yahoo.com>
2024-09-23 10:40:27 -0700

    init commit

---
+++ src/main/java/com/myorg/MyPipelineStack.java
@@ -0,0 +1,24 @@
+package com.myorg;
+
+import software.constructs.Construct;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+// import software.amazon.awscdk.Duration;
+// import software.amazon.awscdk.services.sqs.Queue;
+
+public class MyPipelineStack extends Stack {
+    public MyPipelineStack(final Construct scope, final String id) {
+        this(scope, id, null);
+    }
+
+    public MyPipelineStack(final Construct scope, final String id, final StackProps props) {
+        super(scope, id, props);
+
+        // The code that defines your stack goes here
+
+        // example resource
+        // final Queue queue = Queue.Builder.create(this, "MyPipelineQueue")
+        //         .visibilityTimeout(Duration.seconds(300))
+        //         .build();
+    }
+}
